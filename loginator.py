75dad34fe4181c4271bfa8fab34b64815cc6605f
qubusp <jakob.r.jakov@gmail.com>
2018-12-05 09:53:17 +0100

    FIXING UP

--- loginator.py
+++ loginator.py
@@ -7,26 +7,12 @@ import docker
 
 
 
-recr = boto3.client('ecr',
-    region_name='eu-central-1'
+ecr = boto3.client('ecr',
+    region_name='eu-west-1'
 )
-
-
 
 response = ecr.get_authorization_token()
 raw_data=response['authorizationData'][0]['authorizationToken']
 user, decoded_data=b64decode(raw_data).decode('UTF-8').split(":")
-
-
-config.load_kube_config()
-v1 = client.CoreV1Api()
-namespace = 'default'
-metadata = {'name': 'aws-login', 'namespace': 'default'}
-data = {'user': 'AWS', 'token': decoded_data}
-api_version = 'v1'
-kind = 'Secret'
-body = client.V1Secret(api_version, data , kind, metadata, type='kubernetes.io/tls')
-api_response = v1.create_namespaced_secret(namespace, body, async=True)
-pprint(api_response)
 docker_client = docker.from_env()
-docker_client.login(username=AWS, password=decoded_data, registry=<registry>)
+docker_client.login(username='AWS', password=decoded_data, registry='163690719035.dkr.ecr.eu-west-1.amazonaws.com')
